42369b236fafd212d73aaa7094f08e8657894765
Joshua Bisch <joshuamichael@gmail.com>
2015-06-23 07:38:32 -0700

    Updated Closed VC
    Added more timer logic and visual styling.

--- Brnch/ClosedViewController.swift
+++ Brnch/ClosedViewController.swift
@@ -12,9 +12,11 @@ class ClosedViewController: UIViewController {
     
     
     @IBOutlet weak var countdownLabel: UILabel!
+    @IBOutlet weak var countdownNumber: UILabel!
+    @IBOutlet weak var spinnerImage: UIImageView!
     
     var timer = NSTimer()
-    
+    var rotating = false
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,6 +24,8 @@ class ClosedViewController: UIViewController {
         //Declare Timer
         timer = NSTimer.scheduledTimerWithTimeInterval(0, target:self, selector: Selector("updateCountdown"), userInfo: nil, repeats: true)
         
+        rotating = true
+        rotateOnce()
     }
 
     override func didReceiveMemoryWarning() {
@@ -39,8 +43,8 @@ class ClosedViewController: UIViewController {
         let nextBrunchComponents = NSDateComponents()
         nextBrunchComponents.year = 2015
         nextBrunchComponents.month = 6
-        nextBrunchComponents.day = 27
-        nextBrunchComponents.hour = 11
+        nextBrunchComponents.day = 23
+        nextBrunchComponents.hour = 8
         nextBrunchComponents.minute = 0
         let nextBrunch = userCalendar.dateFromComponents(nextBrunchComponents)!
         
@@ -60,18 +64,43 @@ class ClosedViewController: UIViewController {
             options: nil)
         
         if difference.day == 0 && difference.hour == 0 && difference.minute < 1 {
-            self.countdownLabel.text = "\(difference.second) seconds to go"
+            self.countdownNumber.text = "\(difference.second)"
+            self.countdownLabel.text = "SECONDS TO GO"
+        } else if difference.day == 0 && difference.hour == 0 && difference.minute == 1 {
+            self.countdownNumber.text = "\(difference.minute)"
+            self.countdownLabel.text = "MINUTE TO GO"
         } else if difference.day == 0 && difference.hour == 0 && difference.minute > 1 {
-            self.countdownLabel.text = "\(difference.minute) minutes to go"
+            self.countdownNumber.text = "\(difference.minute)"
+            self.countdownLabel.text = "MINUTES TO GO"
         } else if difference.day == 0 && difference.hour == 1 {
-            self.countdownLabel.text = "\(difference.hour) hour to go"
+            self.countdownNumber.text = "\(difference.hour)"
+            self.countdownLabel.text = "HOUR TO GO"
         } else if difference.day == 0 && difference.hour > 1 {
-            self.countdownLabel.text = "\(difference.hour) hours to go"
+            self.countdownNumber.text = "\(difference.hour)"
+            self.countdownLabel.text = "HOURS TO GO"
         } else if difference.day == 1 {
-            self.countdownLabel.text = "\(difference.day) day to go"
+            self.countdownNumber.text = "\(difference.day)"
+            self.countdownLabel.text = "DAY TO GO"
         } else if difference.day > 1 {
-            self.countdownLabel.text = "\(difference.day) days to go"
+            self.countdownLabel.text = "\(difference.day)"
+            self.countdownLabel.text = "DAYS TO GO"
         }
+    }
+    
+    func rotateOnce() {
+        UIView.animateWithDuration(1.8,
+            delay: 0.0,
+            options: .CurveLinear,
+            animations: {self.spinnerImage.transform = CGAffineTransformRotate(self.spinnerImage.transform, 3.1415926)},
+            completion: {finished in self.rotateAgain()})
+    }
+    
+    func rotateAgain() {
+        UIView.animateWithDuration(1.8,
+            delay: 0.0,
+            options: .CurveLinear,
+            animations: {self.spinnerImage.transform = CGAffineTransformRotate(self.spinnerImage.transform, 3.1415926)},
+            completion: {finished in if self.rotating { self.rotateOnce() }})
     }
     
 
